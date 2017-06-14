1a81962e64025cb11782023832fc326676d72381
Rodja Trappe <rodja@perpetual-mobile.de>
2017-06-14 08:32:18 +0200

    rendering alerts

---
+++ Tests/TestRendering.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DemoApp;
+using NUnit.Framework;
+using UserFlow;
+namespace Tests
+{
+	public class TestRendering : UserTest<App>
+	{
+		[Test]
+		public void TestAlertRendering()
+		{
+			Tap("Alert");
+			ShouldSee("Message");
+
+			Assert.That(Render(), Is.EqualTo("Alert\nMessage\n\n[] [Ok]"));
+		}
+
+	}
+}
