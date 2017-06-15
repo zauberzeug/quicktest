669d51a176298d04570dd5347d644807bc61f998
Rodja Trappe <rodja@perpetual-mobile.de>
2017-06-15 12:15:12 +0200

    element somehow may be null

--- UserFlow/ElementSearch.cs
+++ UserFlow/ElementSearch.cs
@@ -48,7 +48,7 @@ namespace UserFlow
 				(element as Entry)?.Text == text ||
 				((element as Entry)?.Placeholder == text && string.IsNullOrEmpty((element as Entry)?.Text)) ||
 				(element as TextCell)?.Text == text ||
-				element.AutomationId == text;
+				element?.AutomationId == text;
 		}
 
 		static void AddTapGestureRecognizers(Element sourceElement, IEnumerable<ElementInfo> result)
