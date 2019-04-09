dd9ea034f435bd1deb91fa83b381f206e4d23534
Sascha Willam <sascha@perpetual-mobile.de>
2019-04-09 16:28:40 +0200

    Add still ignored test

---
+++ Tests/TabbedTitleViewPageTests.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp;
+using NUnit.Framework;
+using QuickTest;
+
+namespace Tests
+{
+    public class TabbedTitleViewPageTests : QuickTest<App>
+    {
+        [SetUp]
+        protected override void SetUp()
+        {
+            base.SetUp();
+
+            Launch(new App());
+            OpenMenu("TabbedTitleViewPage");
+        }
+
+        [Test, Ignore("Not working yet")]
+        public void Test()
+        {
+            ShouldSee("Some content");
+
+            Tap("TitleViewPage");
+            Tap("TitleViewLabel");
+            ShouldSee("Tapped on Label");
+        }
+    }
+}
