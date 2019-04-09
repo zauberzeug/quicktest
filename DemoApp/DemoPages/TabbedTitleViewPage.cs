6f46c4f26e56cb6dde0deab2d07cf900abcd0fd4
Sascha Willam <sascha@perpetual-mobile.de>
2019-04-09 16:14:45 +0200

    Add new TabbedTitleViewPage

---
+++ DemoApp/DemoPages/TabbedTitleViewPage.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace DemoApp
+{
+    public class TabbedTitleViewPage : TabbedPage
+    {
+        public TabbedTitleViewPage()
+        {
+            Title = "TabbedTitleViewPage";
+
+            Children.Add(CreateContentPage());
+            Children.Add(CreateTitleViewPage());
+        }
+
+        TitleViewPage CreateTitleViewPage()
+        {
+            var titleViewPage = new TitleViewPage();
+            titleViewPage.Appearing += (s, e) => NavigationPage.SetTitleView(this, titleViewPage.TitleView);
+            titleViewPage.Disappearing += (s, e) => NavigationPage.SetTitleView(this, null);
+            return titleViewPage;
+        }
+
+        ContentPage CreateContentPage()
+        {
+            return new ContentPage {
+                Title = "ContentPage",
+                Content = new Label { Text = "Some content" }
+            };
+        }
+    }
+}
