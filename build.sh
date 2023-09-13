3bb85e2f0015c3bb794c5be9ffea2c67af1df454
Stephan Palmer <stephan@zauberzeug.com>
2023-09-13 11:47:27 +0200

    Update build script so that other packages sources configured on build system do not interfere.

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ nuget restore QuickTest.sln || exit 1
 msbuild /p:Configuration=Release QuickTest/QuickTest.csproj || exit 1
 msbuild /p:Configuration=Release Tests/Tests.csproj || exit 1
 
-pushd Tests && nuget install Nunit.Runners && popd
+pushd Tests && nuget install -Source nuget.org Nunit.Runners && popd
 export MONO_IOMAP=all # this fixes slash, backslash path separator problems within nunit test runner
 NUNIT=(Tests/NUnit.ConsoleRunner.*/tools/nunit3-console.exe)
 mono ${NUNIT[0]} --config=Release "Tests/Tests.csproj" || exit 1
